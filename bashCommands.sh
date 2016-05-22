93ae8aa9c2760a257cde207294d23a996626cee5
Vince Bloise <vbloise3@verizon.net>
2016-05-22 15:03:11 -0400

    Added query of stack command to bash script for set-up commands

--- bashCommands.sh
+++ bashCommands.sh
@@ -2,3 +2,6 @@
 
 ##start a cloudformation template containing an EC2 instance
 aws cloudformation create-stack --stack-name jenkins-recovery --template-url https://s3.amazonaws.com/awsinaction/chapter11/recovery.json --parameters ParameterKey=JenkinsAdminPassword,ParameterValue=myjenkins
+
+##get output of template create command
+aws cloudformation describe-stacks --stack-name jenkins-recovery --query Stacks[0].Outputs
